87648a61937f2c0193e03dc7524a206947a379b9
Alex Dehnert <adehnert@mit.edu>
2017-01-15 00:34:03 -0500

    Sort of attempt to pull PyZephyr from Github
    PyPI's PyZephyr is too old, so we need to use the Github version. I tried a few
    things, which didn't seem to work, but here's some of the detritus. It appears
    that pip (or whatever) pretty reasonably doesn't want to pull packages from
    arbitrary places, so we may be out of luck until fixing PyZephyr on PyPI.

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
 
     install_requires=['lxml'],
     extras_require={
-        'zephyr': ['PyZephyr'],
+        #'zephyr': ['-r requirements.zephyr.txt'],
         #'test': ['coverage'],
     },
 
