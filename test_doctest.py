5516194105519091296ebb3392293348f71dd550
Alex Dehnert <adehnert@mit.edu>
2017-01-15 19:10:55 -0500

    Work around the Py2/3 unicode repr() differences
    See https://dirkjan.ochtman.nl/writing/2014/07/06/single-source-python-23-doctests.html
    for details on the problem.

--- test_doctest.py
+++ test_doctest.py
@@ -1,10 +1,20 @@
-import unittest
 import doctest
+import re
+import sys
+import unittest
 
 import chiron
 
+# From https://dirkjan.ochtman.nl/writing/2014/07/06/single-source-python-23-doctests.html
+class Py23DocChecker(doctest.OutputChecker):
+  def check_output(self, want, got, optionflags):
+    if sys.version_info[0] > 2:
+      want = re.sub("u'(.*?)'", "'\\1'", want)
+      want = re.sub('u"(.*?)"', '"\\1"', want)
+    return doctest.OutputChecker.check_output(self, want, got, optionflags)
+
 def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocTestSuite(chiron))
+    tests.addTests(doctest.DocTestSuite(chiron, checker=Py23DocChecker()))
     return tests
 load_tests.__test__ = False
 
