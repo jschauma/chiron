212c7a8a0647b9ff7927a9f6fb7d25ba463e16e5
Alex Dehnert <adehnert@mit.edu>
2019-10-02 04:00:40 -0400

    Use different path for deployed Chiron

--- deploy/run.sh
+++ deploy/run.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
+deploy=`dirname "$0"`
+. $deploy/../../../bin/activate
+
 export PYTHONIOENCODING=utf8
 while true; do
     zwrite -c chiron -i prod -m "Starting chiron instance... (args: \"$@\")"
-    ~/chiron/main.py "$@"
+    $deploy/../main.py "$@"
     zwrite -c chiron -i prod -m "Finished running chiron instance (args: \"$@\")."
     sleep 60
 done
